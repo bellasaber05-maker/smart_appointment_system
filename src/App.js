a0db6b22e89c795bee6a5fce421c705f62cde9eb
bellasaber05-maker <bellasaber05@gmail.com>
2026-01-20 03:12:22 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Book from "./pages/Book";
-import MyAppointments from "./pages/MyAppointments";
-import Contact from "./pages/Contact";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/book" element={<Book />} />
-        <Route path="/myappointments" element={<MyAppointments />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
